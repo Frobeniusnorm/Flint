5783c8e3b6bfef5086302a49428a0b07b0bbae8a
Frobeniusnorm@ilmgard <david.schwarzbeck@fau.de>
2022-08-19 17:55:38 +0200

    added license to files

--- flint.h
+++ flint.h
@@ -1,3 +1,17 @@
+/* Copyright 2022 David Schwarzbeck
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
 #ifndef FLINT_H
 #define FLINT_H
 #include "src/logger.hpp"
@@ -9,10 +23,8 @@
 #endif
 /*
   This is the basic header file and implementation of Flint, to be compatible
-  with as many languages as possible. Though it is written in C++ for object
-  orientation it is as close to C as possible to guarantee compatibility,
-  nevertheless it is NOT compatible to C. Maybe in the future i will rewrite
-  this header to add C compatibility.
+  with as many languages as possible. I have never tested it with the bare C
+  compiler, but it should be C compatible.
 
   Flints Execution structure represents an AST, so that each Graph may be
   compiled to a specific OpenCL program
