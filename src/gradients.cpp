77299c35195c9d32732be5e6f7c0c4b6fc495bd4
Frobeniusnorm <david.schwarzbeck@fau.de>
2023-03-09 23:24:07 +0100

    corrected gradient of repeat

--- src/gradients.cpp
+++ src/gradients.cpp
@@ -261,17 +261,18 @@ static FGraphNode *local_gradient(FGraphNode *y, FGraphNode *dx,
       return nullptr;
   }
   case FREPEAT: {
-    // TODO i think this is wrong, we should sum up all repeated dimensions
     FGraphNode *a = y->predecessors[0];
     std::vector<long> start(y->operation->dimensions, 0);
     std::vector<long> end(y->operation->dimensions);
+    long rep_mul = 1;
     for (int i = 0; i < start.size(); i++) {
       if (y->operation->shape[i] != a->operation->shape[i])
         end[i] = y->operation->shape[i] / a->operation->shape[i];
       else
         end[i] = y->operation->shape[i];
+      rep_mul *= (y->operation->shape[i] / a->operation->shape[i]);
     }
-    return fslice(prev_adj, start.data(), end.data());
+    return fmul(fslice(prev_adj, start.data(), end.data()), rep_mul);
   }
   case FTRANSPOSE: {
     int *transp = ((int *)y->operation->additional_data);
