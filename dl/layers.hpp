9fce49d5a00ecd0f46557a62b1c6c33810acca89
Frobeniusnorm <david.schwarzbeck@fau.de>
2023-06-07 09:54:47 +0200

    Abstract Layer class

--- dl/layers.hpp
+++ dl/layers.hpp
@@ -11,3 +11,71 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License. */
+
+#include <flint/flint.hpp>
+namespace LayerHelper {
+/**
+ * FOR INTERNAL USE ONLY
+ * builds an compile-time linked list of Tensor pointer
+ */
+template <unsigned int index, int... w> class WeightRef {};
+
+template <unsigned int index, int n> struct WeightRef<index, n> {
+  Tensor<double, n> *weight = nullptr;
+  template <unsigned int k, unsigned int f>
+  void set_weight(Tensor<double, f> *w) {
+    if (k != index)
+      flogging(F_ERROR, "Invalid weight index " + std::to_string(k) +
+                            "! Only " + std::to_string(index + 1) + " exist!");
+    static_assert(
+        f == n,
+        "Could not set weight, wrong number of dimensions! Please specify your "
+        "template for the Layer super class correctly!");
+    weight = w;
+  }
+};
+
+template <unsigned int index, int n, int... wn>
+struct WeightRef<index, n, wn...> {
+  Tensor<double, n> *weight = nullptr;
+  WeightRef<index + 1, wn...> others;
+  template <unsigned int k, unsigned int f>
+  void set_weight(Tensor<double, f> *w) {
+    if constexpr (k == index) {
+      static_assert(f == n, "Could not set weight, wrong number of dimensions! "
+                            "Please specify your "
+                            "template for the Layer super class correctly!");
+      weight = w;
+    } else
+      others.template set_weight<k>(w);
+  }
+};
+} // namespace LayerHelper
+/**
+ * Virtual super class of all Layer implementations with type safe weight
+ * management capabilities. The variadic template describes the dimensionality
+ * of the individual weights i.e. a `Layer<3,4,5>` has three weights:
+ * `Tensor<double, 3>`, `Tensor<double, 4>`, `Tensor<double, 5>`.
+ */
+template <int... wn> class Layer {
+  LayerHelper::WeightRef<0, wn...> weight_refs;
+
+  template <unsigned int index, unsigned int n>
+  void init_weights(Tensor<double, n> *t) {
+    weight_refs.template set_weight<index>(t);
+  }
+  template <unsigned int index, unsigned int n, typename... args>
+  void init_weights(Tensor<double, n> *t, args &...weights) {
+    weight_refs.template set_weight<index>(t);
+    init_weights<index + 1>(weights...);
+  }
+
+public:
+  Layer() = default;
+  template <typename... args> Layer(args... weights) {
+    init_weights<0>(weights...);
+  }
+  template <int index, int dim> void set_weight(Tensor<double, dim> *t) {
+    weight_refs.template set_weight<index>(t);
+  }
+};
