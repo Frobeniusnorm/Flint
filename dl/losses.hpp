08739c0fca38b219f0483a05c9031ceef48d47f3
Frobeniusnorm <david.schwarzbeck@fau.de>
2023-06-23 22:11:11 +0200

    first losses

--- dl/losses.hpp
+++ dl/losses.hpp
@@ -11,3 +11,26 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License. */
+#include "layers.hpp"
+struct Loss {
+  // to fulfill generic layer
+  void generate_optimizer(OptimizerFactory *factory) {}
+  // to fulfill generic layer
+  template <typename T, unsigned int dim>
+  void optimize_weights(const Tensor<T, dim> &error) {}
+  static constexpr FType transform_type(FType t) { return t; }
+  static constexpr unsigned int transform_dimensionality(unsigned int n) {
+    return n;
+  }
+};
+
+class SoftMax : public Loss {
+  int ax = -1;
+public:
+  template <typename T, unsigned int n>
+  Tensor<T, n> forward(Tensor<T, n> &in) {
+    // TODO this can be done way more accurate and efficient
+    Tensor<T, n> exp = Flint::constant((T)2.71828, in.get_shape()).pow(in);
+    return exp / exp.reduce_sum(ax < 0 ? in.get_shape()[n - ax] : in.get_shape()[ax]);
+  }
+};
