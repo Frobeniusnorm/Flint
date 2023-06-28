aa98e9ce421690488fa824797fa48c67c0b00453
Frobeniusnorm <david.schwarzbeck@fau.de>
2023-06-28 09:55:07 +0200

    image loading works now

--- examples/dl_test_2/mnist.cpp
+++ examples/dl_test_2/mnist.cpp
@@ -1,55 +1,61 @@
 #include "../../dl/activations.hpp"
 #include "../../dl/layers.hpp"
 #include "../../dl/models.hpp"
+#include <cstring>
 #include <flint/flint.h>
 #include <flint/flint.hpp>
 #include <flint/flint_helper.hpp>
+#include <stdexcept>
 
-static int read_int_at_pos(char *buff, int pos) {
-  return (buff[pos] << 3) & (buff[pos + 1] << 2) & (buff[pos + 2] << 1) &
-         buff[pos + 3];
+int reverseInt(int i) {
+  unsigned char c1, c2, c3, c4;
+  c1 = i & 255;
+  c2 = (i >> 8) & 255;
+  c3 = (i >> 16) & 255;
+  c4 = (i >> 24) & 255;
+  return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
 }
-
-static Tensor<float, 3> load_mnist_images(const std::string path) {
-  char *buffer;
-  long size;
+static Tensor<double, 3> load_mnist_images(const std::string path) {
   using namespace std;
-  ifstream file("train-images.idx3-ubyte", ios::in | ios::binary | ios::ate);
-  size = file.tellg();
-  std::cout << size << std::endl;
-  file.seekg(0, ios::beg);
-  buffer = new char[size];
-  file.read(buffer, size);
-  file.close();
-  unsigned int magic = read_int_at_pos(&buffer[0], 0);
-  std::cout << magic << std::endl;
-  unsigned int no = read_int_at_pos(buffer, 4);
-  int h = read_int_at_pos(buffer, 8), w = read_int_at_pos(buffer, 12);
-  std::cout << no << ", " << h << ", " << w << std::endl;
-  std::vector<float> data(no * h * w);
-  for (int i = 0; i < no; i++) {
-    for (int j = 0; j < h; j++) {
-      for (int k = 0; k < w; k++) {
-        char value = buffer[12 + i * h * w + j * w + i];
-        data[i * h * w + j * w + i] = value / 255.0;
-        std::cout << value << std::endl;
+  errno = 0;
+  ifstream file(path);
+  if (file.is_open()) {
+    int magic_number = 0;
+    int no = 0;
+    int h = 0;
+    int w = 0;
+    file.read((char *)&magic_number, sizeof(magic_number));
+    magic_number = reverseInt(magic_number);
+    file.read((char *)&no, sizeof(no));
+    no = reverseInt(no);
+    file.read((char *)&h, sizeof(h));
+    h = reverseInt(h);
+    file.read((char *)&w, sizeof(w));
+    w = reverseInt(w);
+    std::vector<double> data(no * h * w);
+    for (int i = 0; i < no; i++) {
+      for (int j = 0; j < h; j++) {
+        for (int k = 0; k < w; k++) {
+          unsigned char value;
+          file.read((char *)&value, 1);
+          data[i * h * w + j * w + k] = value / 255.0;
+        }
       }
     }
-  }
-  delete buffer;
-  std::array<size_t, 3> shape{(size_t)no, (size_t)h, (size_t)w};
-  return Tensor<float, 3>(fCreateGraph(data.data(), no * h * w, F_FLOAT64,
-                                       shape.data(), shape.size()),
-                          shape);
+    std::array<size_t, 3> shape{(size_t)no, (size_t)h, (size_t)w};
+    return Tensor<double, 3>(fCreateGraph(data.data(), no * h * w, F_FLOAT64,
+                                         shape.data(), shape.size()),
+                            shape);
+  } else
+    throw std::runtime_error("Could not load file! Please download it from "
+                             "http://yann.lecun.com/exdb/mnist/");
 }
 static Tensor<int, 2> load_mnist_labels(const std::string path) {}
 
 // download and extract to the desired folder from
 // http://yann.lecun.com/exdb/mnist/
 int main() {
-  FlintContext _;
-  Tensor<float, 3> ims = load_mnist_images("train-images.idx3-ubyte");
-  Tensor<float, 3> fst_img =
-      ims.slice(TensorRange(0, 1)).flattened(0).expand(3, 3);
-  Flint::store_image(fst_img, "tst-img.png", FImageFormat::F_PNG);
+  FlintContext _(FLINT_BACKEND_ONLY_CPU);
+  read_mnist("train-images.idx3-ubyte");
+  Tensor<double, 3> ims = load_mnist_images("train-images.idx3-ubyte");
 }
