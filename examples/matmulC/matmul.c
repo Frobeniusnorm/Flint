ba2c2c623092b107b4c8d32af2e6797cdc1fecdb
Lukas Boehm <lukas.l.boehm@fau.de>
2023-09-13 02:13:06 +0200

    :bug: fix example a little bit

--- examples/matmulC/matmul.c
+++ examples/matmulC/matmul.c
@@ -3,19 +3,30 @@
 #include <flint/flint.h>
 
 int main(void) {
-    flintInit(0);
+    flintInit(FLINT_BACKEND_ONLY_CPU);
+    fEnableEagerExecution();
+    fSetLoggingLevel(F_VERBOSE);
 
-    float data1[] = {1,2,3,4};
-    double data2[] = {4,3,2,1};
-    double shape[] = {2,2};
-    FGraphNode *g1 = fCreateGraph(&data1[0], 4, F_FLOAT32,  &shape[0], 2);
-    FGraphNode *g2 = fCreateGraph(&data2[0], 4, F_FLOAT64, &shape[0], 2);
-    FGraphNode *mm = fExecuteGraph(fmatmul(g1, g2));
-    FResultData *rd = mm->result_data;
-    double* result = rd->data;
-//    double result_shape = {mm->operation->shape[0], mm->operation->shape[1]};
+    int32_t data1[] = {1, 3, 3, 4};
+    int32_t data2[] = {4, 9, 2, 56};
+    size_t shape[] = {2, 2};
+    FGraphNode *g1 = fCreateGraph(&data1[0], 4, F_INT32, &shape[0], 2);
+    FGraphNode *g2 = fCreateGraph(&data2[0], 4, F_INT32, &shape[0], 2);
+    FGraphNode *add = fadd_g(g1, g2);
+    FGraphNode *res = fCalculateResult(add);
+    int32_t *result = res->result_data->data;
+    size_t result_shape[] = {res->operation.shape[0], res->operation.shape[1]};
 
-    fprintf("result: %f", &result);
+    size_t repr_len;
+    // FIXME: why does it segfault when i use rd here?
+    char *repr = fserialize(res, &repr_len);
+    for (int i = 0; i < 4; i++) printf("res%i: %d\n", i, result[i]);
 
+    puts(repr);
+    printf("repr_len: %d\n", repr_len);
+
+    free(repr);
+    fFreeGraph(add);
+    flintCleanup();
     return 0;
 }
