07e7c363c3d7c3b8a10a6cfae8e64a5ef174f0e3
Frobeniusnorm <david@frobeniusnorm.de>
2024-03-11 00:38:29 +0100

    Layer Norm

--- dl/layers/normalization.hpp
+++ dl/layers/normalization.hpp
@@ -14,6 +14,7 @@
 #ifndef FLINT_NORMALIZATION
 #define FLINT_NORMALIZATION
 #include "../layers.hpp"
+#include <limits>
 
 /** Randomly sets some values in the input to 0 with a probability of `p`.
  * Reduces over fitting. Degenerates to an identity function when `training` is
@@ -29,16 +30,42 @@ class Dropout : public UntrainableLayer {
 			if (!training) {
 				return in;
 			}
-      // s.t. the memory may be reused we remove the handle of in 
-      in.get_graph_node()->reference_counter--;
+			// s.t. the memory may be reused we remove the handle of in
+			in.get_graph_node()->reference_counter--;
 			auto result = in.dropout(p);
-      // avoid in freeing memory on destruction
-      in.set_graph_node(nullptr);
-      return result;
+			// avoid in freeing memory on destruction
+			in.set_graph_node(nullptr);
+			return result;
 		}
 		std::string name() override { return "Dropout"; }
 		std::string description() override {
 			return "p = " + std::to_string(p);
+		}
+};
+template <unsigned int n, typename F = float>
+struct LayerNorm : public Layer<F, n, n> {
+		LayerNorm(std::array<size_t, n> normalized_shape)
+			: Layer<F, n, n>(Flint::constant_array(1.0f, normalized_shape),
+							 Flint::constant_array(0.0f, normalized_shape)) {}
+		template <typename T, unsigned int k>
+		Tensor<T, k> forward(Tensor<T, k> &in) {
+			// flatten last dimensions for reductions
+			using namespace std;
+			array<size_t, k - n + 1> flat_shape;
+			for (int i = 0; i < k - n; i++)
+				flat_shape[i] = in.get_shape()[i];
+			flat_shape[k - n] = 1;
+			for (int i = 0; i < n; i++)
+				flat_shape[k - n] *= in.get_shape()[k - n + i];
+			Tensor<T, k - n + 1> flat = in.reshape_array(flat_shape);
+			Tensor<T, k - n> mean =
+				(flat / (T)flat.get_shape()[k - n]).reduce_sum();
+			Tensor<T, k - n> var =
+				((flat - mean).pow(2) / (T)flat.get_shape()[k - n])
+					.reduce_sum();
+			return ((in - mean) / (var + numeric_limits<F>::epsilon()).sqrt()) *
+					   Layer<F, n, n>::template get_weight<0>() +
+				   Layer<F, n, n>::template get_weight<1>();
 		}
 };
 #endif
